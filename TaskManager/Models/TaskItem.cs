10844a1389c643ddb7c25bd6d9b9324915f8a870
Minnelli Rodriguez <Minnellirodriguezmercedes0826@gmail.com>
2026-07-31 20:57:54 -0400

    Add task validation rules

--- TaskManager/Models/TaskItem.cs
+++ TaskManager/Models/TaskItem.cs
@@ -1,19 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace TaskManager.Models 
+namespace TaskManager.Models
 {
     public class TaskItem
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "El titulo es obligatorio")]
-        [StringLength(100)]
+
+        [Required(ErrorMessage = "El título es obligatorio")]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "El título debe tener entre 3 y 50 caracteres")]
         public string Title { get; set; } = string.Empty;
 
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
         [StringLength(300)]
         public string Description { get; set; } = string.Empty;
 
+
         public bool Completed { get; set; }
     }
-
 }
